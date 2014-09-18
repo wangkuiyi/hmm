1cc459ed9e3538b8d0e32725af709eba5e32df2d
Yi Wang <yi.wang.2005@gmail.com>
2014-09-18 09:55:26 -0700

    Add TestNewInstance

--- core/core_test.go
+++ core/core_test.go
@@ -6,50 +6,47 @@ import (
 )
 
 var (
-	dacheng = NewInstance(
-		[][]Observed{
-			[]Observed{
-				Observed{"vice": 1, "president": 1},
-				Observed{"applied": 1, "predictive": 1}},
-			[]Observed{
-				Observed{"head": 1, "solution": 1, "strategy": 1},
-				Observed{"opera": 1, "solutions": 1}},
-			[]Observed{
-				Observed{"founder": 1},
-				Observed{"helping": 1}},
-			[]Observed{
-				Observed{"senior": 1, "manager": 1},
-				Observed{"linkedin": 1}}},
-		[]int{8, 0, 1, 1})
+	kDachengObs = [][]Observed{
+		[]Observed{
+			Observed{"vice": 1, "president": 1},
+			Observed{"applied": 1, "predictive": 1}},
+		[]Observed{
+			Observed{"head": 1, "solution": 1, "strategy": 1},
+			Observed{"opera": 1, "solutions": 1}},
+		[]Observed{
+			Observed{"founder": 1},
+			Observed{"helping": 1}},
+		[]Observed{
+			Observed{"senior": 1, "manager": 1},
+			Observed{"linkedin": 1}}}
+	kDachengPeriods = []int{8, 0, 1, 1}
 
-	yiwang = NewInstance(
-		[][]Observed{
-			[]Observed{
-				Observed{"software": 1, "engineer": 1},
-				Observed{"google": 1}},
-			[]Observed{
-				Observed{"engineering": 1, "director": 1},
-				Observed{"tencent": 1, "ads": 1}},
-			[]Observed{
-				Observed{"data": 1, "scientist": 1},
-				Observed{"linkedin": 1}}},
-		[]int{3, 4, 0})
+	kYiObs = [][]Observed{
+		[]Observed{
+			Observed{"software": 1, "engineer": 1},
+			Observed{"google": 1}},
+		[]Observed{
+			Observed{"engineering": 1, "director": 1},
+			Observed{"tencent": 1, "ads": 1}},
+		[]Observed{
+			Observed{"data": 1, "scientist": 1},
+			Observed{"linkedin": 1}}}
+	kYiPeriods = []int{3, 4, 0}
 
-	gwang = NewInstance(
-		[][]Observed{
-			[]Observed{
-				Observed{"cofounder": 1},
-				Observed{"scissorsfly": 1}},
-			[]Observed{
-				Observed{"sr": 1, "associate": 1, "business": 1, "analyst": 1},
-				Observed{"linkedin": 1}}},
-		[]int{1, 1})
+	kGuanObs = [][]Observed{
+		[]Observed{
+			Observed{"cofounder": 1},
+			Observed{"scissorsfly": 1}},
+		[]Observed{
+			Observed{"sr": 1, "associate": 1, "business": 1, "analyst": 1},
+			Observed{"linkedin": 1}}}
+	kGuanPeriods = []int{1, 1}
 )
 
 func TestBuildInstanceIndex(t *testing.T) {
-	index := buildInstanceIndex(dacheng.Periods)
+	index := buildInstanceIndex(kDachengPeriods)
 	exp := 0
-	for _, l := range dacheng.Periods {
+	for _, l := range kDachengPeriods {
 		exp += l
 	}
 	if len(index) != exp {
@@ -58,6 +55,21 @@ func TestBuildInstanceIndex(t *testing.T) {
 	truth := []int{0, 0, 0, 0, 0, 0, 0, 0, 2, 3}
 	if !reflect.DeepEqual(index, truth) {
 		t.Errorf("Expecting %d, got %d", truth, index)
+	}
+}
+
+func TestNewInstance(t *testing.T) {
+	dacheng := NewInstance(kDachengObs, kDachengPeriods)
+	if !reflect.DeepEqual(dacheng.Obs, kDachengObs) {
+		t.Errorf("Expecting %v, got %v", kDachengObs, dacheng.Obs)
+	}
+	if !reflect.DeepEqual(dacheng.Periods, kDachengPeriods) {
+		t.Errorf("Expecting %v, got %v", kDachengPeriods, dacheng.Periods)
+	}
+
+	null := NewInstance([][]Observed{nil, nil}, []int{0, 0})
+	if null != nil {
+		t.Errorf("Expecting nil, got %v", null)
 	}
 }
 
